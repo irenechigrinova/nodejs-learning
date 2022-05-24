d5765a6ed8b96a44dc2df2ac148edfe881036395
ichigrinova <ichigrinova@calltouch.net>
2022-05-24 11:30:15 +0300

    module 2 | user service type fixed

--- src/module2/types/user.types.ts
+++ src/module2/types/user.types.ts
@@ -5,15 +5,3 @@ export type TUser = {
   age: number;
   isDeleted: boolean;
 };
-
-export type TUserService = {
-  findUsers: (params: Partial<TUser>) => Promise<TUser[]>;
-  findUserById: (id: string) => Promise<TUser | undefined>;
-  createUser: (user: Partial<TUser>) => Promise<TUser>;
-  updateUser: (id: string, user: Partial<TUser>) => Promise<TUser | undefined>;
-  softDeleteUser: (id: string) => Promise<boolean>;
-  sortUsers: (
-    users: TUser[],
-    params: { login: string; limit: number; offset: number }
-  ) => { result: TUser[]; total: number };
-};
